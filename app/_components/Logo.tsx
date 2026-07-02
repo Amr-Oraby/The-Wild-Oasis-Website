1cd2608afc9b81581759c0f3215b2683fd52717d
oraby <amrorapy50@gmail.com>
2026-07-02 18:22:28 +0900

    Create Logo

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
-
+import Image from "next/image";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img
+      <Image
         src="/logo.png"
-        className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 lg:h-16 lg:w-16"
+        width={60}
+        height={60}
         alt="The Wild Oasis logo"
+        className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 lg:h-16 lg:w-16"
       />
       <span className="text-md md:text-xl font-semibold text-primary-100">
         The Wild Oasis
