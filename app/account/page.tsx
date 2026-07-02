b9464054833dbde200c39417a3308920187faa35
oraby <amrorapy50@gmail.com>
2026-07-03 03:33:02 +0900

    Create Account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Account",
 };
-function page() {
-  return <h1>Account page</h1>;
+function page({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>Welcome, Amr</h1>
+      <div className="space-x-3 bg-blue-600 w-fit">
+        <Link href="account/reservations">reservations</Link>
+        <Link href="account/profile">profile</Link>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 }
 
 export default page;
