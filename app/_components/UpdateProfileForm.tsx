feec1f25ba948f5ab66638922a365b519ced8004
oraby <amrorapy50@gmail.com>
2026-07-15 01:50:01 +0900

    Add UpdateProfileForm

--- app/_components/UpdateProfileForm.tsx
+++ app/_components/UpdateProfileForm.tsx
@@ -1,9 +1,7 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
 
 function UpdateProfileForm({ children }: { children: React.ReactNode }) {
-  const [count, setCount] = useState();
   // CHANGE
   const countryFlag = "/pt.jpg";
 
