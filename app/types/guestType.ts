5a51a88e468e1d911357597f50bc47020b04f8cf
oraby <amrorapy50@gmail.com>
2026-07-19 00:58:24 +0900

    Update types

--- app/types/guestType.ts
+++ app/types/guestType.ts
@@ -1,9 +1,9 @@
 export type guestType = {
   id?: number;
   created_at?: string;
-  fullName: string | null;
-  email: string | null;
-  nationalID?: string | null;
-  nationality?: string | null;
-  countryFlag?: string | null;
+  fullName: string;
+  email: string;
+  nationalID?: string;
+  nationality?: string;
+  countryFlag?: string;
 };
